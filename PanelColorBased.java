77db0f07b49cc074153210761ebaa900243e56ba
pq <ybjqx3340@gmail.com>
2015-05-11 16:11:54 -0400

    fixed accidental hit issue by adding numOfUps

--- PanelColorBased.java
+++ PanelColorBased.java
@@ -124,6 +124,10 @@ public class PanelColorBased extends JPanel
         ConditionButton confirmSlider = new ConditionButton();
         confirmSlider.setText("Looks Good");
         options.add(confirmSlider);
+        String[] layoutLst = new String[]{"Xylophone Standard", "Xylophone Extended"};
+        JComboBox layoutList = new JComboBox<>(layoutLst);
+        layoutList.setSelectedIndex(0);
+        options.add(layoutList);
         mainPanelColorBased.add(options);
         GUIframe.setVisible(true);
         GUIframe.setSize(1200, 280);
@@ -133,8 +137,9 @@ public class PanelColorBased extends JPanel
         midi.run(); // start a new thread
 
         /** define note **/
-        final int C3 = 50, D3 = 70, E3 = 90, F3 = 110, G3 = 130, A4 = 150, B4 = 170, C4 = 190;
-        int[] colorToNote = new int[191];
+        final int C3 = 50, D3 = 60, E3 = 70, F3 = 80, G3 = 90, A4 = 100, B4 = 110, C4 = 120; // basic layout
+        final int D4 = 130, E4 = 140, F4 = 150, G4 = 160, A5 = 170; // extended layout
+        int[] colorToNote = new int[171];
         colorToNote[C3] = 60;
         colorToNote[D3] = 62;
         colorToNote[E3] = 64;
@@ -143,6 +148,11 @@ public class PanelColorBased extends JPanel
         colorToNote[A4] = 69;
         colorToNote[B4] = 71;
         colorToNote[C4] = 72;
+        colorToNote[D4] = 74; // beginning of extended layout
+        colorToNote[E4] = 76;
+        colorToNote[F4] = 77;
+        colorToNote[G4] = 79;
+        colorToNote[A5] = 81;
 
         /** img processing **/
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -206,8 +216,8 @@ public class PanelColorBased extends JPanel
             Mat temp = new Mat();
             paperMask.copyTo(temp);
             ArrayList<MatOfPoint> theContour = new ArrayList<>(2);
-            Mat tempHierachy = new Mat();
-            Imgproc.findContours(temp, theContour, tempHierachy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            Mat tempHierarchy = new Mat();
+            Imgproc.findContours(temp, theContour, tempHierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
             Core.fillConvexPoly(paperMask, theContour.get(0), white8UC1);
 
             // update GUI
@@ -295,7 +305,7 @@ public class PanelColorBased extends JPanel
                 filteredContoursDisplay = Mat.zeros(contoursToFind.size(), contoursToFind.type());
                 for (int i = 0; i < filteredContours.size(); i++) {
                     Imgproc.drawContours(filteredContoursDisplay, filteredContours, i, contourColorGray, -1);
-                    contourColorGray.val[0] += 20; // update color
+                    contourColorGray.val[0] += 10; // update color
                     if (contourColorGray.val[0] > 255)
                         contourColorGray.val[0] = 50.0;
                 } // C:50 D:70 E:90 F:110 G:130 A:150 B:170 C:190
@@ -308,7 +318,7 @@ public class PanelColorBased extends JPanel
              * Now, done learning layout **********************************************************************
              **************************************************************************************************/
 
-            double[][] lowHigh1 = (double[][]) FileIO.deserialize("lof_orange");
+            double[][] lowHigh1 = (double[][]) FileIO.deserialize("high_orange");
             double[][] lowHigh2 = (double[][]) FileIO.deserialize("lof_red"); // enables snd mallet
 //            double[][] lowHigh2 = null;
             Scalar low1 = new Scalar(lowHigh1[0]);
@@ -335,6 +345,7 @@ public class PanelColorBased extends JPanel
 
             double direction1 = 0; // direction the mallet moves
             double direction2 = 0;
+            int numOfUps2 = 0;
             Point hitIndicator = new Point(20, 20);
             final int ZERO_VAL = 5;
             int zeroCounter1 = ZERO_VAL, zeroCounter2 = ZERO_VAL;
@@ -379,14 +390,14 @@ public class PanelColorBased extends JPanel
                 /** stabilize centroid **/
                 if ((currCentroid1.x - lastCentroid1.x) * (currCentroid1.x - lastCentroid1.x) +
                         (currCentroid1.y - lastCentroid1.y) * (currCentroid1.y - lastCentroid1.y)
-                        < 30) {
+                        < 20) {
                     currCentroid1.x = lastCentroid1.x; // just jittering, ignore
                     currCentroid1.y = lastCentroid1.y;
                 }
                 if (lowHigh2 != null) {
                     if ((currCentroid2.x - lastCentroid2.x) * (currCentroid2.x - lastCentroid2.x) +
                             (currCentroid2.y - lastCentroid2.y) * (currCentroid2.y - lastCentroid2.y)
-                            < 30) {
+                            < 20) {
                         currCentroid2.x = lastCentroid2.x; // just jittering, ignore
                         currCentroid2.y = lastCentroid2.y;
                     }
@@ -428,12 +439,15 @@ public class PanelColorBased extends JPanel
                     if (currCentroid2.y - lastCentroid2.y > 0) { // keeps going down
                         zeroCounter2 = ZERO_VAL;
                         direction2 = currCentroid2.y - lastCentroid2.y;
-                    } else if (currCentroid2.y - lastCentroid2.y <= 0 && direction2 > 0) { // switching direction to up
+                        numOfUps2++;
+                    } else if (currCentroid2.y - lastCentroid2.y <= 0 && direction2 > 0 && numOfUps2 > 1) { // switching direction to up
                         midi.sound(colorToNote[(int) (filteredContoursDisplay.get((int) currCentroid2.y, (int) currCentroid2.x))[0]]);
+                        System.out.println("hit");
                         if (filteredContoursDisplay.get((int) currCentroid2.y, (int) currCentroid2.x)[0] == 0)
                             Core.circle(currHSVFrame, hitIndicator, 10, red, -1);
                         direction2 = currCentroid2.y - lastCentroid2.y;
                         zeroCounter2 = ZERO_VAL;
+                        numOfUps2 = 0;
                         // highlight on hitting note
                         keyColorVal2 = (int) (filteredContoursDisplay.get((int) currCentroid2.y, (int) currCentroid2.x))[0];
                         for (row = 0; row < R; row++)
@@ -446,9 +460,11 @@ public class PanelColorBased extends JPanel
                             zeroCounter2--;
                         } else {
                             direction2 = 0;
+                            numOfUps2 = 0;
                         }
                     }
                 }
+                System.out.println(direction2);
 
                 /** update left canvas **/
                 currBuffImg = matToBufferedImage(currBGRFrame);
